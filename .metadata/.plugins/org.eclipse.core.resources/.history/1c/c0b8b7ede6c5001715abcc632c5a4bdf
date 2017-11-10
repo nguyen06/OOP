519505eea76713b23cec30bf8cb3071b553ef280
nguyen06 <nnthinh88@gmail.com>
2017-11-09 23:17:07 -0800

    next commnit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1c/c0b8b7ede6c5001715abcc632c5a4bdf
@@ -0,0 +1,5 @@
+package DevelopmentExercises;
+
+public class guessingGame {
+
+}
--- CharaterAndString/src/DevelopmentExercises/guessingGame.java
+++ CharaterAndString/src/DevelopmentExercises/guessingGame.java
@@ -1,5 +1,18 @@
 package DevelopmentExercises;
-
+/*
+ * problem description:
+ * Write a word guessing game. The game is played by two players, 
+ * each player take a turn in guessing the secret word entered by the other player.
+ * Ask the first player to enter a secret word. After a secret word is entered,
+ * display a hint that consists of a row of dashes, one for each letter in the 
+ * secret word. Then ask the second player to guess a letter in the secret word.
+ * if the leter is in the secret word, replace the dashes in the hint with the letter 
+ * at all positions where this letter occurs in the word. if the letter does not appear in
+ * the word, the number of incorrect guesses is incremented by 1. The second player keeps 
+ * guessing letters until either
+ * 	+ the player guesses all the letters in the word
+ *  + the player makes 10 incorrect guess.  
+ */
 public class guessingGame {
 
 }
